a7ca5017b444171e24e89de9d816a0e2cb18ba42
Jagritidas123 <jagritid764@gmail.com>
2026-01-11 10:34:59 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+
+
+public class Hello {
+    // THIS IS BLOCK
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        // ANYTHING INSIDE THE DOUBLE QOUTE IS A STRING
+
+        System.out.println(4 + 3);
+        // THIS IS PERFORMING ADDITION
+        System.out.println("4 + 3");
+        // THIS IS ALSO A STRING 
+    }
+
+}
