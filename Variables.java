8efa933281202f5c9395a03c53ac5b42f65e4f4c
Jagritidas123 <jagritid764@gmail.com>
2026-01-11 10:35:29 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,12 @@
+public class Variables{
+    public static void main(String[] args) {
+        int age = 10;
+
+
+        // int is a data type
+        // Variable is space in the memory where a value is stored
+
+        
+        System.out.println(age);
+    }
+}
