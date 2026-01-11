a9ab6cf5b4eb89695c590c3144af54fbee54dcc8
Jagritidas123 <jagritid764@gmail.com>
2026-01-11 10:35:44 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,8 @@
+public class Datatype{
+    public static void main(String[] args) {
+        // THESE ARE THE PRIMITIVE DATATYPE
+        // int, char, float, boolan, double, long , short, byte
+        int count = 12;
+        long countlong = count;
+    }
+}
